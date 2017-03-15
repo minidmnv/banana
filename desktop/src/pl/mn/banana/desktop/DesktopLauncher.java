123f8232dc67a987b684c998cb6d8ae75fe6e22c
mnicinski <minidmnv@gmail.com>
2017-03-15 09:11:19 +0100

    Initial commit

---
+++ desktop/src/pl/mn/banana/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pl.mn.banana.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pl.mn.banana.BananaGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BananaGame(), config);
+	}
+}
